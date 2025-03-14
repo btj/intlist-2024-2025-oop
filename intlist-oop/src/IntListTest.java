e89948da4fe18b0da89e900b1cf3215e64de8cee
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2025-03-14 11:19:50 +0100

    Fixed test suite

--- intlist-oop/src/IntListTest.java
+++ intlist-oop/src/IntListTest.java
@@ -9,19 +9,19 @@ class IntListTest {
 
 	@Test
 	void testConstructor() {
-		assertEquals(new int[] {10, 20, 30}, myIntList.getElements());
+		assertArrayEquals(new int[] {10, 20, 30}, myIntList.getElements());
 	}
 	
 	@Test
 	void testAdd() {
 		myIntList.add(40);
-		assertEquals(new int[] {10, 20, 30, 40}, myIntList.getElements());
+		assertArrayEquals(new int[] {10, 20, 30, 40}, myIntList.getElements());
 	}
 	
 	@Test
 	void testRemoveLast() {
 		myIntList.removeLast();
-		assertEquals(new int[] {10, 20}, myIntList.getElements());
+		assertArrayEquals(new int[] {10, 20}, myIntList.getElements());
 	}
 
 }
